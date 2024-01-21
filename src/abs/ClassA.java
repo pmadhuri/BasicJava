dd1ccdbb6f59ed635e3136038e68c052fcddd61a
madhuri <guravmadhuri93@gmail.com>
2024-01-21 17:06:21 +0530

    Basic 2 line add

--- src/abs/ClassA.java
+++ src/abs/ClassA.java
@@ -4,6 +4,8 @@ public class ClassA {
 
 	public static void main(String[] args) {
 		System.out.println(smethod("madhuri1234"));
+		System.out.println("madhuri added 1");
+		System.out.println("madhuri added 2");
 		
 
 	}
